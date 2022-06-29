5e0cfd55ab3037fcd6a1e7bc0089abad416c963d
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-06-29 21:59:08 +0530

    t-shirts page

---
+++ pages/tshirts.js
@@ -0,0 +1,113 @@
+import Link from 'next/link'
+import React from 'react'
+
+const tshirts = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-7 sm:px-24 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4 justify-evenly">
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default tshirts
