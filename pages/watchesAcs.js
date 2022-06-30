b2d5581036012d4aa9ef0c5d10309e04b82b8513
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-06-30 20:44:57 +0530

    Adding Watches page

---
+++ pages/watchesAcs.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import Link from 'next/Link'
+
+const WatchesAcs = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-7 sm:px-24 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4 justify-evenly">
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+            <Link href={'/product/tshirts'}>
+              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                <a className="block relative rounded overflow-hidden">
+                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/k6l2vm80/watch/r/z/n/arrow-black-daniel-jubile-original-imafjyyhwkedgeja.jpeg?q=70" />
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+                  <p className="mt-1">₹499.00.</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default WatchesAcs
