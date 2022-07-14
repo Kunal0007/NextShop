27c52d78cad9ba4c939706d12e1b49223f187471
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-14 23:14:23 +0530

    BuyNow feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,21 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
   const [cart, setCart] = useState({});
   const [cartTotal, setCartTotal] = useState(0);
+  const router = useRouter();
 
   useEffect(() => {
-    const cart = localStorage.getItem('cart');
+    console.log("useEffect");
     try {
-      if (cart) {
-        setCart(JSON.parse(cart));
+      if (localStorage.getItem("cart")) {
+        setCart(JSON.parse(localStorage.getItem("cart")));
+        saveCart(JSON.parse(localStorage.getItem("cart")))
       }
     } catch (error) {
       console.error(error);
@@ -20,35 +23,63 @@ function MyApp({ Component, pageProps }) {
     }
   }, [])
 
-  const saveCart = (cart) => {
+  const saveCart = (mycart) => {
     console.log("saving cart");
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('cart', JSON.stringify(mycart));
+
+    let total = 0;
+    let keys = Object.keys(mycart);
+    for (let i = 0; i < keys.length; i++) {
+      total += mycart[keys[i]].itemPrice * mycart[keys[i]].itemQty;
+    }
+    setCartTotal(total);
   }
 
   const addtoCart = (itemCode, itemName, itemPrice, itemQty) => {
+    console.log("adding to cart");
     let newCart = cart;
+    let subTotal = itemPrice;
     if (itemCode in newCart) {
       newCart[itemCode].itemQty += itemQty;
+      newCart[itemCode].subTotal = newCart[itemCode].itemPrice * newCart[itemCode].itemQty;
     }
     else {
       newCart[itemCode] = {
         itemCode,
         itemName,
         itemPrice,
-        itemQty
+        itemQty,
+        subTotal
       }
     }
-    console.log(newCart);
     setCart(newCart);
     saveCart(newCart);
+  }
+
+  const buyNow = (itemCode, itemName, itemPrice, itemQty) => {
+    let subTotal = itemPrice;
+    let newCart = {
+      itemCode: {
+        itemCode,
+        itemName,
+        itemPrice,
+        itemQty,
+        subTotal
+      }
+    };
+
+    setCart(newCart);
+    saveCart(newCart);
+    router.push("/checkout");
   }
 
   const removefromCart = (itemCode, itemName, itemPrice, itemQty) => {
     let newCart = cart;
     if (itemCode in newCart) {
       newCart[itemCode].itemQty -= itemQty;
+      newCart[itemCode].subTotal = newCart[itemCode].itemPrice * newCart[itemCode].itemQty;
     }
-    if(newCart[itemCode].itemQty <= 0) {
+    if (newCart[itemCode].itemQty <= 0) {
       delete newCart[itemCode];
     }
     setCart(newCart);
@@ -62,9 +93,9 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Navbar cart={cart} saveCart={saveCart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} />
-      <Component {...pageProps} cart={cart} saveCart={saveCart} addtoCart={addtoCart} 
-      removefromCart={removefromCart} clearCart={clearCart} />
+      <Navbar key={cartTotal} cart={cart} saveCart={saveCart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} />
+      <Component {...pageProps} cart={cart} saveCart={saveCart} addtoCart={addtoCart} buyNow={buyNow}
+        removefromCart={removefromCart} clearCart={clearCart} cartTotal={cartTotal} />
       <Footer />
     </>
   )
