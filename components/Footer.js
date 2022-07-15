5f32202e577747d5066482792c2ec5cb28f0ff58
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-15 16:06:25 +0530

    footer link update

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/Link'
 import React from 'react'
 
 const Footer = () => {
@@ -6,11 +7,28 @@ const Footer = () => {
       <footer className="bg-gray-800 text-white body-font shadow-lg">
         <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-            <p className="mt-2 text-sm text-white">Air plant banjo lyft occupy retro adaptogen indego</p>
+            <p className="mt-2 text-sm text-white">Shop Latest and Next Generation Clothes & Accessories</p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Shop</h2>
+              <nav className="list-none mb-10">
+                <li>
+                  <Link href={'/tshirts'}><a className="text-white hover:text-white">T-Shirts</a></Link>
+                </li>
+                <li>
+                  <Link href={'/causalwear'}><a className="text-white hover:text-white">Casual Wear</a></Link>
+                </li>
+                <li>
+                  <Link href={'/footwear'}><a className="text-white hover:text-white">Foot Wear</a></Link>
+                </li>
+                <li>
+                  <Link href={'/watchesAcs'}><a className="text-white hover:text-white">Watches and Accessories</a></Link>
+                </li>
+              </nav>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">About</h2>
               <nav className="list-none mb-10">
                 <li>
                   <a className="text-white hover:text-white">First Link</a>
@@ -27,7 +45,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Policy</h2>
               <nav className="list-none mb-10">
                 <li>
                   <a className="text-white hover:text-white">First Link</a>
@@ -44,24 +62,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-              <nav className="list-none mb-10">
-                <li>
-                  <a className="text-white hover:text-white">First Link</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-white">Second Link</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-white">Third Link</a>
-                </li>
-                <li>
-                  <a className="text-white hover:text-white">Fourth Link</a>
-                </li>
-              </nav>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+              <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">Socials</h2>
               <nav className="list-none mb-10">
                 <li>
                   <a className="text-white hover:text-white">First Link</a>
@@ -81,9 +82,7 @@ const Footer = () => {
         </div>
         <div className="bg-gray-700">
           <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p className="text-white text-sm text-center sm:text-left">© 2020 Tailblocks —
-              <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-white ml-1" target="_blank">@knyttneve</a>
-            </p>
+            <p className="text-white text-sm text-center sm:text-left">© 2022 NextShop</p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
               <a className="text-white">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
