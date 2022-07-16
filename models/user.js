0f561f885d099498321355d33a0d4d4ccebc738e
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-16 18:04:46 +0530

    Model update

---
+++ models/user.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+}, {timestamps: true});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
