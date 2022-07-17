2b4b2e0ff8fb4fb876f5e2bf864c66fbed515e7f
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-17 23:34:21 +0530

    JWT token Added

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,21 +1,26 @@
 import connectDB from "../../middleware/mongoose";
 import User from "../../models/user";
+import CryptoJS from 'crypto-js';
+import jwt from "jsonwebtoken";
 
 const handler = async (req, res) => {
     if (req.method === "POST") {
         console.log(req.body);
-        let user = await User.findOne({email: req.body.email});
-        if(user){
-            if(user.password == req.body.password){
-                res.status(200).json({message: "User Logged In", success: true});
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            let bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+            let decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+            if (decryptedData == req.body.password) {
+                let token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET, {expiresIn: "2d"});
+                res.status(200).json({ message: "User Logged In", success: true, token: token });
             }
             else {
-                res.status(401).json({message: "Invaild Credentials", success: false});
-            } 
+                res.status(401).json({ message: "Invaild Credentials", success: false });
+            }
         }
         else {
-            res.status(401).json({message: "Invaild Credentials", success: false}); 
-        }              
+            res.status(401).json({ message: "Invaild Credentials", success: false });
+        }
     }
     else {
         res.status(405).json({ msg: "Method is not allowed" });
