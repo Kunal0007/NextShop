c7054545456ea894389d75dbaa2a69f2922630f4
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-18 22:18:47 +0530

    api update

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,12 +1,15 @@
 import connectDB from "../../middleware/mongoose";
 import User from "../../models/user";
+import CryptoJS from 'crypto-js';
 
 const handler = async (req, res) => {
     if (req.method === "POST") {
-        console.log(req.body);
-        let user = new User(req.body);
+        const { name, email} = req.body;
+        // Encrypt
+        let ciphertext = CryptoJS.AES.encrypt(req.body.password, 'secret0007').toString();
+        let user = new User({ name, email, password: ciphertext });
         await user.save();
-        res.status(200).json({message: "User account created successfully"});
+        res.status(200).json({ message: "User account created successfully" });
     }
     else {
         res.status(405).json({ msg: "Method is not allowed" });
