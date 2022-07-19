15ebf27f2372a4be445375b424d5e1d87660f8cc
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-19 22:24:40 +0530

    logout function

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,26 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import LoadingBar from 'react-top-loading-bar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
   const [cart, setCart] = useState({});
   const [cartTotal, setCartTotal] = useState(0);
+  const [user, setUser] = useState({value: null});
+  const [key, setKey] = useState(0);
+  const [progress, setProgress] = useState(0)
   const router = useRouter();
 
   useEffect(() => {
     console.log("useEffect");
+    router.events.on('routeChangeStart', () => {
+      setProgress(40);
+    })
+    router.events.on('routeChangeComplete', () => {
+      setProgress(100);
+    })
     try {
       if (localStorage.getItem("cart")) {
         setCart(JSON.parse(localStorage.getItem("cart")));
@@ -21,7 +31,18 @@ function MyApp({ Component, pageProps }) {
       console.error(error);
       localStorage.clear();
     }
-  }, [])
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser({ value: token });
+      setKey(Math.random());
+    }
+  }, [router.events, router.query])
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setUser({ value: null });
+    setKey(Math.random());
+  }
 
   const saveCart = (mycart) => {
     console.log("saving cart");
@@ -93,7 +114,14 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Navbar key={cartTotal} cart={cart} saveCart={saveCart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} />
+      <LoadingBar
+        color='#5856d6'
+        progress={progress}
+        waitingTime={500}
+        height={3}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Navbar key={key} user={user} cart={cart} saveCart={saveCart} addtoCart={addtoCart} removefromCart={removefromCart} clearCart={clearCart} logout={logout} />
       <Component {...pageProps} cart={cart} saveCart={saveCart} addtoCart={addtoCart} buyNow={buyNow}
         removefromCart={removefromCart} clearCart={clearCart} cartTotal={cartTotal} />
       <Footer />
