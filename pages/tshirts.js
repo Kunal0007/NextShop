2a412ee6710f762e4d8f15f44eccb26897ebd86f
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-07-24 20:08:14 +0530

    Update

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,113 +1,50 @@
-import Link from 'next/link'
+import Link from 'next/Link'
 import React from 'react'
+import mongoose from 'mongoose'
+import Product from '../models/product'
 
-const tshirts = () => {
+
+const tshirts = ({ products }) => {
+  console.log(products);
   return (
     <div>
       <section className="text-gray-600 body-font">
-        <div className="container px-7 sm:px-24 py-24 mx-auto">
+        <div className="container px-7 sm:px-24 pb-7 mx-auto">
+          <h1 className='text-center text-black font-bold text-3xl my-10'>T-shirts</h1>
           <div className="flex flex-wrap -m-4 justify-evenly">
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                  <p className="mt-1">₹499.00.</p>
+            {products.map((item) => {
+              return (
+              <Link key={item._id} href={`/product/${item.slug}`}>
+                <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
+                  <a className="block relative rounded overflow-hidden">
+                    <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src={item.img} />
+                  </a>
+                  <div className="mt-4">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">{item.title}</h2>
+                    <p className="mt-1">₹{item.price}.00</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/l2arp8w0/t-shirt/c/w/9/xs-tlorrn-d45-tripr-original-imagdzex5gg6dahf.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
-            <Link href={'/product/tshirts'}>
-              <div className="lg:w-1/5 md:w-1/3 m-2 p-4 w-full shadow-md cursor-pointer">
-                <a className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-cover object-center w-full shadow-md h-full block" src="https://rukminim1.flixcart.com/image/832/832/kzllrbk0/t-shirt/b/6/d/l-bylrn-z31-blive-original-imagbkqgknxgmncw.jpeg?q=70" />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-                  <p className="mt-1">₹499.00.</p>
-                </div>
-              </div>
-            </Link>
+              </Link>
+            )})}
           </div>
         </div>
       </section>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  if (!mongoose.connections[0].readyState) {
+    await mongoose.connect(process.env.MONGO_URI)
+  }
+
+  const products = await Product.find({ category: 'T-shirts' });
+  
+  return {
+    props: { products: JSON.parse(JSON.stringify(products)) }, // will be passed to the page component as props
+  }
 }
 
 export default tshirts
