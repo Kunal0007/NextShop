341f13d2de1182c5e3b651f9896895271dfb82bd
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-08-02 21:50:23 +0530

    variable Update

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -5,7 +5,7 @@ const connectDB = handler => async (req, res) => {
         return handler(req, res);
     }
 
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI)
     return handler(req, res);
 }
 
