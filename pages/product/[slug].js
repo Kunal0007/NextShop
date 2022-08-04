15823512637a9cc5bb7ea2d79a1d5e4615f47fa4
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-08-04 20:24:51 +0530

    Update to check login before buying product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -32,6 +32,25 @@ const Post = ({ addtoCart, buyNow, product, variants }) => {
         setPincode(event.target.value)
     }
 
+    const handlebuyNow = () => {
+        if(!localStorage.getItem('token')){
+            router.push('/login');
+        }
+        else {
+            buyNow(slug, `${product.title}(${product.color}/${product.size})`, product.price, 1)
+        }        
+    }
+
+    const handleAddtoCart = () => {
+        if(!localStorage.getItem('token')){
+            router.push('/login');
+        }
+        else {
+            addtoCart(slug, `${product.title}(${product.color}/${product.size})`, product.price, 1)
+        }        
+    }
+       
+
     const refreshVariants = (newcolor, newsize) => {
         if(!Object.keys(variants[newcolor]).includes(newsize)){
             newsize = Object.keys(variants[newcolor])[0];
@@ -85,8 +104,8 @@ const Post = ({ addtoCart, buyNow, product, variants }) => {
                             </div>
                             <div className="flex border-b-2 border-gray-100 mb-5 pb-5">
                                 <span className="title-font font-medium text-2xl text-gray-900 basis-2/5">₹{product.price}.00</span>
-                                <button onClick={() => { buyNow(slug, `${product.title}(${product.color}/${product.size})`, product.price, 1) }} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">BUY NOW </button>
-                                <button onClick={() => { addtoCart(slug, `${product.title}(${product.color}/${product.size})`, product.price, 1) }} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
+                                <button onClick={handlebuyNow} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">BUY NOW </button>
+                                <button onClick={handleAddtoCart} className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
                                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
