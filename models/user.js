8c54af392c021a146caaa3e99482f4df4ea2a20c
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-08-05 00:42:23 +0530

    Added Cart to user

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    cart: {type: Object, required: true},
 }, {timestamps: true});
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
